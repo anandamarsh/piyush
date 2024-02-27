7847022fc17ea244976f27dedc8cbdc60bf2f371
Amarsh Anand <amarsh.anand@gmail.com>
2024-02-27 17:24:44 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Click Me</Button>
+    </div>
+  );
+}
